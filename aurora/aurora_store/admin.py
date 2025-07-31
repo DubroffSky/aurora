2b09d2dc007703564145315e80148b4b063985fa
Grigorii Dubrovskii <grigorii.dubrovskii@web.de>
2025-07-31 19:46:38 +0200

    set admin panel

--- aurora/aurora_store/admin.py
+++ aurora/aurora_store/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from aurora_store.models import Product, OrderItem, Order
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic.detail import DetailView
+from aurora_store.models import Order
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+    
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created']
+    inlines = [OrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'count']
+
