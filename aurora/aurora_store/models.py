a58c3fb900473d4d96e08d7783e84580f622b3b4
Grigorii Dubrovskii <grigorii.dubrovskii@web.de>
2025-08-21 04:56:58 +0200

    add profile photo to model

--- aurora/aurora_store/models.py
+++ aurora/aurora_store/models.py
@@ -1,8 +1,27 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 
 User = get_user_model()
+
+# Profile model for user photo
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='user_photos/', blank=True, null=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
+
+# Signal to create or update user profile
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
 
 
 class Order(models.Model):
