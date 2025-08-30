2f7301722b46d2d6fe30db8aa6a898b706ad91c4
Grigorii Dubrovskii <grigorii.dubrovskii@web.de>
2025-08-31 01:46:23 +0200

    Add members field

--- aurora/aurora_store/forms.py
+++ aurora/aurora_store/forms.py
@@ -38,9 +38,15 @@ class CustomAuthenticationForm(AuthenticationForm):
 
 # TaskFlow Forms
 class ProjectForm(forms.ModelForm):
+    members = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        widget=forms.SelectMultiple(attrs={'class': 'form-control'})
+    )
+
     class Meta:
         model = Project
-        fields = ['title', 'description', 'status',]
+        fields = ['title', 'description', 'status', 'members']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter project title'}),
             'description': forms.Textarea(attrs={
