5ceaab8299618ccd3f11b16a48af4d0c3e640c1a
Grigorii Dubrovskii <grigorii.dubrovskii@web.de>
2025-09-03 23:31:32 +0200

    remove trash imports

--- aurora/aurora_store/admin.py
+++ aurora/aurora_store/admin.py
@@ -3,8 +3,3 @@ from aurora_store.models import *
 from aurora_store.models import Profile
 # Register Profile model
 admin.site.register(Profile)
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.views.generic.detail import DetailView
-from aurora_store.models import Order
-
-
