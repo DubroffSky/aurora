a472a8a3f23d9545821f18cff8327a07d93b1274
Grigorii Dubrovskii <grigorii.dubrovskii@web.de>
2025-09-07 23:50:00 +0200

    settings update

--- aurora/aurora/settings.py
+++ aurora/aurora/settings.py
@@ -104,8 +104,8 @@ WSGI_APPLICATION = 'aurora.wsgi.application'
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST', 'localhost')
-DB_PORT = os.getenv('DB_PORT', '5432')
+DB_URL = os.getenv('DB_URL')
+DB_PORT = os.getenv('DB_PORT')
 
 DATABASES = {
     "default": {
@@ -113,7 +113,7 @@ DATABASES = {
         "NAME": DB_NAME,
         "USER": DB_USER,
         "PASSWORD": DB_PASSWORD,
-        "HOST": DB_HOST,
+        "URL": DB_URL,
         "PORT": DB_PORT,
         "OPTIONS": {
             "client_encoding": "UTF-8",
