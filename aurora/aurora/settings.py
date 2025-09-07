4ee6a48d092953a287cd72d90a41459461dcc1db
Grigorii Dubrovskii <grigorii.dubrovskii@web.de>
2025-09-07 23:03:57 +0200

    pip freeze

--- aurora/aurora/settings.py
+++ aurora/aurora/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-q61)-8^%!s&*_=r+pe3ws&!4=*(&zl_#gf$tmtoeq^dca!g)p8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
